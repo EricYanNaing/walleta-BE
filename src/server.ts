65b979ad473c4c5cd5bcf6865b931bb6b3dd35b5
ericthedev7 <ericthedev97@gmail.com>
2026-01-05 09:44:21 +0700

    asdfdsf

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import {app} from './app'
 import dotenv from 'dotenv'
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const port = Number(process.env.PORT)|| 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+    console.log("DB host:", new URL(process.env.DATABASE_URL!).host);
+console.log("Direct host:", new URL(process.env.DIRECT_URL!).host);
 })
